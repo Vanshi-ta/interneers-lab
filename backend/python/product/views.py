312c4fdb06af2b514d9d6fca2db2519b2bff290a
Vanshi-ta <vanshitasuryavanshi62@gmail.com>
2026-03-06 01:12:14 +0530

    week2 all task

--- backend/python/product/views.py
+++ backend/python/product/views.py
@@ -7,10 +7,14 @@
 
 from django.http import JsonResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 
+
+# In-memory product store
 products = []
 current_id = 1
 
+@csrf_exempt
 def product_list(request):
     global current_id
 
@@ -20,8 +24,36 @@ def product_list(request):
     
     #POST (create)
     if request.method == "POST":
-        data = json.loads(request.body)
-        product = {
+        try:
+            data = json.loads(request.body)
+            
+            # Basic validations
+            required_fields = [
+                "name",
+                "description",
+                "category",
+                "price",
+                "brand",
+                "warehouse_quantity",
+            ]
+
+            #check all fileds are present
+            for field in required_fields:
+                if field not in data:
+                    return JsonResponse(
+                        {"error": f"{field} is required"}, status=400
+                    )
+
+            #check price and quantity are valid
+            if data["price"] < 0:
+                return JsonResponse({"error": "Price must be positive"}, status=400)
+
+            if data["warehouse_quantity"] < 0:
+                return JsonResponse(
+                    {"error": "Quantity cannot be negative"}, status=400
+                )
+            
+            product = {
                 "id": current_id,
                 "name": data["name"],
                 "description": data["description"],
@@ -30,17 +62,24 @@ def product_list(request):
                 "brand": data["brand"],
                 "warehouse_quantity": data["warehouse_quantity"],
             }
-        products.append(product)
-        current_id += 1
+            products.append(product)
+            current_id += 1
+            return JsonResponse(product, status=201)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+        
 
 def product_detail(request, product_id):
     product = [p for p in products if p["id"] == product_id]
     if not product:
         return JsonResponse({"error": "Product not found"}, status=404)
+    
     #DELETE (remove)
     if request.method == "DELETE":
         products.remove(product)
         return JsonResponse({"message": "Product deleted successfully"})
+    
     #PUT (update)
     if request.method == "PUT":
         data = json.loads(request.body)
