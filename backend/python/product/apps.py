eeded8533bc370bece0ef649c84cc4ff4a21db0c
Vanshi-ta <vanshitasuryavanshi62@gmail.com>
2026-03-20 15:02:39 +0530

    Seed script to run on server startup

--- backend/python/product/apps.py
+++ backend/python/product/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ProductConfig(AppConfig):
     name = 'product'
+
+    def ready(self):
+        from scripts.seed_categories import run_seed
+        run_seed()
